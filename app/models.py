ba179459bb468aeaf8e731ee52b4339c1b01b02e
Hadi Almubarak <hadi.almubarak3@gmail.com>
2026-07-25 06:53:23 +0300

    Add User SQLAlchemy model

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, DateTime, func
+from app.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), unique=True, nullable=False)
+    password = Column(String(64), nullable=False)
+    created_at = Column(DateTime, server_default=func.current_timestamp())
